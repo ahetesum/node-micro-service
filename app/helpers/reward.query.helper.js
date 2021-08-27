7884052eab72bdc137f6cc15d9bc52bac7cdfa61
ahetesum <rajkumars@bigcity.in>
2021-08-27 18:24:57 +0530

    Initial Project Commited

--- app/helpers/reward.query.helper.js
+++ app/helpers/reward.query.helper.js
@@ -1,1 +1,3 @@
 module.exports.allBrandCategoryQuery= "SELECT id,category_name,imagepath,program_id FROM reward_categories";
+
+module.exports.brandCategoryQueryId= "SELECT id,category_name,imagepath,program_id FROM reward_categories";
--- app/services/reward.service.js
+++ app/services/reward.service.js
@@ -4,25 +4,38 @@ const {dbPool} = require("../../config/db.config");
 
 module.exports.getAllBrandCategory = async()=> { 
     return new Promise(resolve => {
-    dbPool.connect()
-    .then((client) => {
-        client.query(allBrandCategoryQuery)
-            .then(res => {
-                resolve(res);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    })
-    .catch(err => {
-        console.error(err);
-        resolve({mesage:"Error While Fetching Data"})
+        dbPool.connect()
+        .then((client) => {
+            client.query(allBrandCategoryQuery)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            resolve({mesage:"Error While Fetching Data"})
+        })
     });
-
-});
 };
 
 module.exports.getBrandCategory = (id)=> { 
-
-    return rewards.find(x=>x.id==id);
+    return new Promise(resolve => {
+        dbPool.connect()
+        .then((client) => {
+            client.query(allBrandCategoryQuery)
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            resolve({mesage:"Error While Fetching Data"})
+        })
+    });
 };
